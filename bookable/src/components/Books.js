4cb3bbefec2a20630ec3dc8496562040d4c64cbf
Kasandra Darwin <kasandradarwin@gmail.com>
2022-12-01 03:46:40 +0000

    added some api calls and books

--- bookable/src/components/Books.js
+++ bookable/src/components/Books.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export default function Books(){
+export default function Books(props){
 
-    return(
-        
-    )
-
-}
+    return (
+        <div>
+          <h1>These books are from the API</h1>
+          {props.books.map((book) => {
+            console.log(book)
+            return (
+              <div key={book.id}>
+                <h2>{book.title}</h2>
+                <p>{book.body}</p>
+                
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
